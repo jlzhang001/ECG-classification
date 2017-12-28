d13642821822ce141e5f03b71adc503e602eee9f
Max F <mfedorchuck@gmail.com>
2017-12-28 18:00:40 +0200

    modified main files

--- DataLoad.py
+++ DataLoad.py
@@ -2,23 +2,22 @@ import numpy as np
 import glob
 import wfdb
 
-#import matplotlib.pyplot as plt
-#import math
-
-
-#import scipy.stats
-#import scipy.signal
-
-def fload(FileNames, WinSize):
-	"""Input Arguments:
-		FILENAMES - pointer to the directory and files
-	Output Arguments:
+def fload(FileNames, WinSize, fs):
+	"""
+	Function for reading .atr and .dat files from the physiobank databases
 	
+	Input Arguments:
+		FILENAMES - pointer to the directory and files
+		WINSIZE - size of the chunks of input signal in sec.
+		
+	Output Arguments:
+		X - Array of the input data for further analysis
+		Y - Keys (labels) for classification
 	Example:
 	X, Y = DataLoad.fload(FileNames='RowData/*.dat', Winsize=10)
 	"""
 	#Some necessary settings 
-	SigSize = WinSize * 360	
+	SigSize = WinSize * fs	#For frequency of a signal f=360Hz
 	X = np.array(np.zeros([SigSize])) 
 	Y = np.array(np.zeros([1]))
 	IsNormal = 1
@@ -53,7 +52,7 @@ def fload(FileNames, WinSize):
 				
 			#Checking - is our annotation symbols are ok
 			for item in annotation.symbol[FirstAnn:LastAnn]:
-				if item not in "~+N/fQ?()ptu'sT*D=@":
+				if item not in "~+N/|fQ?()ptu^'`sT*D=@":
 					IsNormal -= 1
 			# If annotation symbols are ok - stack to Y 'one'
 			if IsNormal == 1:
@@ -67,85 +66,4 @@ def fload(FileNames, WinSize):
 	#Y = annotation
 	#annotation.sample
 	#annotation.symbol
-
-	return X[1:,:], Y[1:,:]
-	
-	
-#	
-#def sshow(S):
-#	"""Displaying first 3 EEG signals with index A1-LE.
-#	Displaying only if [Signal] is not an empty array"""
-#	m = 1.2 #multiplyer for plot y limits
-#	#print(S.shape)
-#	plt.figure(1)
-#	
-#	if S.any():
-#		Sig_length = len(S.transpose())
-#		
-#		ax1 = plt.subplot(3, 1, 1)
-#		ax1.plot(range(Sig_length), S[0,:])
-#		ax1.set_ylim([np.min(S[0:2,:])*m, np.max(S[0:2,:])*m])
-#		ax1.set_title('First three EEG signals (A1-LE)')
-#		ax1.xaxis.set_visible(False)
-#		
-#		ax2 = plt.subplot(3, 1, 2)
-#		ax2.plot(range(Sig_length), S[1,:])
-#		ax2.set_ylim([np.min(S[0:2,:])*m, np.max(S[0:2,:])*m])
-#		ax2.xaxis.set_visible(False)
-#		
-#		ax3 = plt.subplot(313)
-#		ax3.plot(range(Sig_length), S[2,:])
-#		ax3.set_ylim([np.min(S[0:2,:])*m, np.max(S[0:2,:])*m])
-#		plt.show
-#		
-#	else:
-#		print('\n Nothing to show. Array is empty :( \n')
-#
-#def cuton(S, WinSize = 100):
-#	#WinSize = int(WinSize)
-#	Pices = int(math.ceil(len(S.transpose()) / WinSize) - 1)
-#	
-##	print(Pices)
-##	print(WinSize)
-##	print(len(S))
-#	CutSig = np.empty((len(S), Pices, WinSize))
-#	
-#	for i in range(len(S)):
-#		for j in range(Pices):
-#			CutSig[i,j,:] = np.array(S[i, j*WinSize:(j+1)*WinSize])
-#	
-#	return CutSig
-#						
-#def cutshow(Sig, N = 1):
-#	
-#	S_merge = np.concatenate((Sig[N,0,:], Sig[N,1,:], Sig[N,2,:]))
-#	Ymin = np.min(S_merge) * 1.1
-#	Ymax = np.max(S_merge) + np.abs(np.max(S_merge) * 0.1)
-#
-#	plt.figure(2)
-#
-#	ax1 = plt.subplot(2, 3, 1)
-#	ax1.plot(range(len(Sig[N,0,:])), Sig[N,0,:])
-#	ax1.set_ylim([Ymin, Ymax])
-#	ax1.xaxis.set_visible(False)
-#	
-#	ax2 = plt.subplot(2, 3, 2)
-#	ax2.plot(range(len(Sig[N,1,:])), Sig[N,1,:])
-#	ax2.set_ylim([Ymin, Ymax])
-#	ax2.xaxis.set_visible(False)
-#	ax2.yaxis.set_visible(False)
-#	
-#	ax3 = plt.subplot(2, 3, 3)
-#	ax3.plot(range(len(Sig[N,2,:])), Sig[N,2,:])
-#	ax3.set_ylim([Ymin, Ymax])
-#	ax3.xaxis.set_visible(False)
-#	ax3.yaxis.set_visible(False)
-#
-#	
-#	ax4 = plt.subplot(2, 1, 2) #here - connected signal
-#	ax4.plot(range(len(S_merge.transpose())), S_merge)
-#	ax4.set_ylim([Ymin, Ymax])
-#	ax4.set_xlim([0, len(S_merge.transpose())])
-#	ax4.xaxis.set_visible(True)
-#	
-#	plt.show
+	return X[1:,:], Y[1:,:], annotation
--- Main.py
+++ Main.py
@@ -1,13 +1,17 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(666)  # for reproducibility
 
-
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
 
 import DataLoad
 #import Features
 
-X, Y = DataLoad.fload(FileNames='RowData/*.dat', WinSize=10)
-
+X, Y, annotation = DataLoad.fload(FileNames='RowData/*.dat', WinSize=10, fs=360)
+print('Input data has been completely parsed and loaded!')
 plt.plot(X[100,:])
 plt.show()
 plt.plot(X[101,:])
@@ -15,14 +19,16 @@ plt.show()
 plt.plot(X[102,:])
 plt.show()
 
-
 print('X.shape: ', X.shape)
 
 print('Y.shape: ', Y.shape)
+print('Ok segments:', Y[Y==1].shape[0], 'from', Y.shape[0])
 
-#Y.sample
-#Y.symbol
-#																	
-##найти нейронки для тайм-сириес
-#
-#arythmia detection in - publications
+#print(annotation.symbol[:100])
+
+#annotation.sample
+#annotation.symbol
+				
+# Implement follow: (X_train, y_train), (X_test, y_test)
+#instruction here: https://elitedatascience.com/keras-tutorial-deep-learning-in-python
+#Task 2: arythmia detection in publications
